1454ac5c70db9d6981faa9c4392864a180c67197
Monknow <juanrodriguezcode@gmail.com>
2021-08-22 12:49:46 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "stack-sharer",
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet"],
+};
